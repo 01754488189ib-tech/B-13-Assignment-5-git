0bdcf7f38926a93c1a264268c9be21829ab1d878
Ibrahim Khalilullah <01754488189ib@gmail.com>
2026-03-09 10:30:08 +0600

    setup card skeleton and frames for home page

--- script/filter.js
+++ script/filter.js
@@ -11,3 +11,27 @@ const filterTap = (status) => {
         });
 };
 
+const displayIssues = (words) => {
+    const wordContainer = document.getElementById("word-container");
+    wordContainer.innerHTML = "";
+
+    document.getElementById('totalStatus').innerText = words.length;
+
+    words.forEach(word => {
+        const card = document.createElement("div");
+
+        const borderColor = word.status === 'open' ? 'border-green-500' : 'border-purple-500';
+
+        card.className = `shadow-md rounded-lg border-r-5 border-t-5 ${borderColor} p-5 mb-5`;
+
+        const borderClass = word.status === 'open' ? 'border-green-500' : 'border-purple-500';
+        
+
+        card.innerHTML = `
+            
+        `;
+        wordContainer.appendChild(card);
+    });
+};
+
+filterTap('all');
